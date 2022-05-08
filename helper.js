bd71e0082bc5a5a21ee027ea0c1b91071295e226
jin1125 <shogo.jinta@gmail.com>
2022-05-08 11:25:56 +0900

    31

--- helper.js
+++ helper.js
@@ -1,13 +1,19 @@
 const fs = require('fs')
 
+const person = {
+  name: 'Mike',
+  age: 30
+}
+
 const read = () => {
-  fs.readFile('./sample.txt','utf8' , (err, data) => {
-      console.log(data);
+  fs.readFile('./sample.json','utf8' , (err, data) => {
+    const person = JSON.parse(data)
+    console.log(person.age);
   })
 }
 
 const write = () => {
-  fs.writeFile('sample.txt', 'hello', () => {
+  fs.writeFile('sample.json', JSON.stringify(person), () => {
     console.log('write!');
   })
 }
