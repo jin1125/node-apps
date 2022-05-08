cc5f2b7a3e734e4af5948d8c4bf1bbd023ac349a
jin1125 <shogo.jinta@gmail.com>
2022-05-08 18:13:39 +0900

    52

--- web-server/index.js
+++ web-server/index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', function (req, res) {
-  console.log(req);
-  res.send('<h1>Top Page!</h1>')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.get('/', function (req, res) {
+//   console.log(req);
+//   res.send('<h1>Top Page!</h1>')
+// })
+
+app.post('/api/v1/quiz', function (req, res) {
+  const answer = req.body.answer
+  if (answer === "2") {
+    res.redirect('/correct.html')
+  } else {
+    res.redirect('/wrong.html')
+  }
 })
 
 app.get('/api/v1/users', function (req, res) {
