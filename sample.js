142ecb2a34da4ac0b52cf6b5b1d701a10c78d7db
jin1125 <shogo.jinta@gmail.com>
2022-05-08 10:06:20 +0900

    20

--- sample.js
+++ sample.js
@@ -2,10 +2,27 @@
 // console.log("app!");
 // console.log("app!");
 
-function test() {
-  console.log('test!');
-}
+// function test() {
+//   console.log('test!');
+// }
 
-test()
-test()
-test()
+// test()
+// test()
+// test()
+
+// setTimeout(()=>{
+//   console.log('timer');
+// }, 1000)
+
+// setTimeout(()=>{
+//   console.log('timer');
+// }, 1000)
+
+// setTimeout(()=>{
+//   console.log('timer');
+// }, 1000)
+
+const { name, add } = require('./helper')
+
+console.log(name)
+console.log(add(2, 3))
